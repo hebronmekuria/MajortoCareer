7c26feea58d50aa7fc4e39f7b0697354a6660458
hebronmekuria <hebron.mekuria@emu.edu>
2024-06-10 18:53:08 -0400

    Related to #9 added an image

--- frontend/majorcareer/src/app/page.tsx
+++ frontend/majorcareer/src/app/page.tsx
@@ -4,7 +4,10 @@ import { Box, ChakraProvider, Heading, Text, HStack, VStack } from "@chakra-ui/r
 import { JobCard } from "./components/JobCard";
 import { DropDown } from "./components/DropDown";
 import { useEffect, useState } from "react";
-import "./styles.css"; // Import the custom CSS
+import "./styles.css";
+import Image from "next/image";
+import logo from './static/investigate.png'
+
 
 interface Job {
   jobtitle: string;
@@ -38,14 +41,21 @@ export default function Home() {
 
   return (
     <ChakraProvider>
-      <Box w='100%' h='100vh' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-        <VStack spacing='28px' align='center' mb='60px'>
+      <Box w='100%' h='100vh' display='flex' flexDirection='column' alignItems='center' mt='10px'>
+        <Box mt='0px' mb='10px'>
+          <Image
+            className="mt-20 w-[300px] h-[300px]"
+            src={logo}
+            alt="logo"
+          />
+        </Box>
+        <VStack spacing='12px' align='center' mb='20px'>
           <Heading>Name of App</Heading>
           <Text width='1000px' overflowWrap='normal' fontSize='20px'>Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.</Text>
         </VStack>
         <DropDown />
-        <Box w='80%' overflowX='auto' className='no-scrollbar'> 
-          <HStack spacing='30px' w='max-content' h='auto'> 
+        <Box w='80%' overflowX='auto' className='no-scrollbar'>
+          <HStack spacing='30px' w='max-content' h='auto'>
             {jobs.map((job) => (
               <JobCard
                 key={job.url}
